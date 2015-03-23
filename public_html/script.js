68466c6c28e3fb1e44abd0fcfca84abdf82bd81a
Taxus <sami.lunnamo@gmail.com>
2015-03-23 21:07:14 +0200

    use dictionary to get words

--- public_html/script.js
+++ public_html/script.js
@@ -2,6 +2,8 @@ var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var index = -1; // alphabet index ^
 var success = document.getElementById("successAnimation"); // html element where the animation is played
 var animInterval; // interval used in animation
+var dictionary;
+
 
 
 // ---------------- Game/general stuff --------------------
@@ -9,7 +11,9 @@ function init() {
     var textArea = document.getElementById("answer");
     textArea.addEventListener('keyup', checkAnswer, false);
     success.style.opacity = 0;
-    displayLetter(randomIndex());
+    loadDictionary(function () {
+        displayWord(randomIndex());    
+    });
     checkAnswer();
 }
 
@@ -18,17 +22,60 @@ function checkAnswer() {
     if (textArea.value.toUpperCase() === alphabet[index]) {
         successAnimationStart();
         textArea.value = "";
-        displayLetter(randomIndex());
+        displayWord(randomIndex());
     }
 }
 
-function displayLetter(index) {
+function loadDictionary(callback) {
+    var xmlhttp;
+
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    }
+    else
+    {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4) {
+            if (xmlhttp.status == 200) {
+                dictionary = xmlhttp.responseText;
+                dictionary = dictionary.split("\n");
+            }
+            else {
+                alert("Something went wrong in getting the dictionary file");
+            }
+        }
+    }
+
+    xmlhttp.open("GET", "words.txt", false);
+    xmlhttp.send();
+    callback();
+}
+
+function displayWord(index) {
+    var word = dictionary[index];
+    word = WordHtml(word);
+
     var letterArea = document.getElementById("letterArea");
-    letterArea.innerHTML = alphabet[index];
+    letterArea.innerHTML = word;
+}
+
+function WordHtml(word) {
+    var result = "";
+    var i = 0;
+    while(i < word.length - 1) {
+        result += "<span id='" + i + "'>" + word.charAt(i) + "</span>";
+        i++
+    }
+    
+
+    return result;
 }
 
 function randomIndex() {
-    index = Math.floor(Math.random() * 26) + 0;
+    index = Math.floor(Math.random() * dictionary.length) + 0;
     return index;
 }
 
