b574db589753a6b43a4798cfbc00eddbd75c5796
Taxus <sami.lunnamo@gmail.com>
2015-04-28 00:10:46 +0300

    Websocket config

--- public_html/websocket.js
+++ public_html/websocket.js
@@ -1,12 +1,13 @@
 var ws;
 
 function CreateWebSocket(callback) {
-	ws = new WebSocket("ws://", "");
+	ws = new WebSocket("ws://localhost:9999/");
 	ws.onopen = function (event) {
 		callback();
 	};
 	ws.onmessage = function (event) {
-		checkSignedAnswer(JSON.parse(event.data));
+		console.log("Message is received..." + event.data)
+		checkSignedAnswer(event.data);
 	}
 }
 
